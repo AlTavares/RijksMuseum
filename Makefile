875e58144bb4b70d0bfd8cdebe2cb86a240460b0
Alexandre Mantovani Tavares <alexandre@live.in>
2020-05-16 15:31:49 +0200

    chore: bootstrap project

---
+++ Makefile
@@ -0,0 +1,13 @@
+appName := $(shell basename $(shell pwd))
+quicktype := quicktype --no-initializers --protocol equatable -l swift --swift-5-support
+
+.PHONY: install
+install:
+	brew update
+	brew bundle
+
+.PHONY: bootstrap
+bootstrap:
+	$(eval path = Sources/RijksMuseum/Config)
+	$(quicktype) $(path)/config.json -o $(path)/Config.swift
+	xcodegen
---
+++ Sources/RijksMuseum/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RijksMuseum
+//
+//  Created by Alexandre Mantovani Tavares on 15/05/20.
+//  Copyright © 2020 Alexandre Mantovani Tavares. All rights reserved.
+//
+
+import SwiftUI
+// fpGQTuED
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
