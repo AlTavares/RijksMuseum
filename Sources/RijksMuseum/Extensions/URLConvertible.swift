d3b507745da9502ffbf92aaa13951e92486601d4
Alexandre Mantovani Tavares <alexandre@live.in>
2020-05-17 14:59:15 +0200

    feat: URLConvertible protocol

---
+++ Sources/RijksMuseum/Extensions/URLConvertible.swift
@@ -0,0 +1,24 @@
+//
+//  URLConvertible.swift
+//  RijksMuseumApp
+//
+//  Created by Alexandre Mantovani Tavares on 17/05/20.
+//
+
+import Foundation
+
+protocol URLConvertible {
+    var url: URL? { get }
+}
+
+extension String: URLConvertible {
+    var url: URL? { URL(string: self) }
+}
+
+extension URL: URLConvertible {
+    var url: URL? { self }
+}
+
+extension Optional where Wrapped == URL {
+    var url: URL? { self }
+}
