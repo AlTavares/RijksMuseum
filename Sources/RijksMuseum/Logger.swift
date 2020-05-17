ea03563a5ea646b17c03bb91e2f5e28422abaa95
Alexandre Mantovani Tavares <alexandre@live.in>
2020-05-17 14:57:38 +0200

    feat: add logger

---
+++ Sources/RijksMuseum/Logger.swift
@@ -0,0 +1,11 @@
+//
+//  Logger.swift
+//  RijksMuseumApp
+//
+//  Created by Alexandre Mantovani Tavares on 17/05/20.
+//
+
+import Logging
+import Foundation
+
+let logger = Logger(label: Bundle.main.bundleIdentifier!)
