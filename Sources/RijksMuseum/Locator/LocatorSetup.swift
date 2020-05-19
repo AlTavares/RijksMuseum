f6fb4e1ac3d85996de6650702366aaf02169b352
Alexandre Mantovani Tavares <alexandre@live.in>
2020-05-19 19:53:26 +0200

    feat: service locator

---
+++ Sources/RijksMuseum/Locator/LocatorSetup.swift
@@ -0,0 +1,23 @@
+//
+//  LocatorSetup.swift
+//  RijksMuseum
+//
+//  Created by Alexandre Mantovani Tavares on 18/05/20.
+//
+
+import Foundation
+
+extension Locator {
+    static var setup: () -> Void = {
+        logger.debug("Setting up dependencies")
+
+        Locator.register(Config.fromBundle())
+
+        Locator.register(CollectionRepository())
+
+        Locator.register(URLSession.shared)
+        Locator.register(HTTPClient())
+
+        return {}
+    }()
+}
